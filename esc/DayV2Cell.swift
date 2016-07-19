91b17c20f345643f524897abfbca4b380077c68e
Riley Pacella <rpacella@umich.edu>
2016-07-20 00:22:51 +0200

    fixed some UI stuff

---
+++ esc/DayV2Cell.swift
@@ -0,0 +1,24 @@
+//
+//  DayV2Cell.swift
+//  esc
+//
+//  Created by Riley Pacella on 7/20/16.
+//  Copyright © 2016 Escape. All rights reserved.
+//
+
+import UIKit
+
+class DayV2Cell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
