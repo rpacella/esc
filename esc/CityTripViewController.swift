3181e87bfedb4f0dab32b4267f70596e0ac1acdf
Riley Pacella <rpacella@umich.edu>
2016-07-19 17:19:08 +0200

    fixed nav controller

--- esc/CityTripViewController.swift
+++ esc/CityTripViewController.swift
@@ -8,19 +8,52 @@
 
 import UIKit
 
-class CityTripViewController: UIViewController {
+class CityTripViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.registerNib(UINib(nibName: "DayV1Cell", bundle: nil), forCellReuseIdentifier: "reuseCell")
+
     }
     
     override func viewWillAppear(animated: Bool) {
-        navigationController?.navigationBarHidden = false
-    }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+         navigationController?.navigationBarHidden = false
     }
     
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        /*
+         if section % 2 == 0 {
+         return DayV1Cell
+         }
+         if section % 2 != 0 {
+         return DayV2Cell
+         }
+         
+         */
+        
+       let cell = tableView.dequeueReusableCellWithIdentifier("reuseCell")
+        return cell!
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        //calulation to see how many days there are in NSCoding
+        /*
+         return CityTripDaysArray.count
+         */
+        
+        return 2
+    }
+    
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        //push to itenerary for day
+        //decode itenerary for day
+    }
 
 }
---
+++ esc/DayV1Cell.swift
@@ -0,0 +1,28 @@
+//
+//  DayV1Cell.swift
+//  esc
+//
+//  Created by Riley Pacella on 7/19/16.
+//  Copyright © 2016 Escape. All rights reserved.
+//
+
+import UIKit
+
+class DayV1Cell: UITableViewCell {
+
+//    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
