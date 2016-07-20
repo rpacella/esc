9abfc602efbc88a0626205cdcc7d21838685e8e0
msl3939 <msl3939@gmail.com>
2016-07-20 16:34:15 +0200

    fixed backgrounds

--- esc/RouteViewController.swift
+++ esc/RouteViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 import MapKit
 
+class Restaurant: NSObject, MKAnnotation {
+    var title: String? = ""
+    var coordinate: CLLocationCoordinate2D = CLLocationCoordinate2D()
+}
+
 class RouteViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var tableView: UITableView!
 
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,23 +49,81 @@ class RouteViewController: UIViewController, MKMapViewDelegate, CLLocationManage
             NSForegroundColorAttributeName: UIColor.blackColor()], forState: UIControlState.Normal)
         
         navigationItem.setLeftBarButtonItems([backbutton, backText], animated: false)
-        
         navigationItem.setRightBarButtonItems([routeLabel, listButton], animated: true)
         
+        let location = CLLocationCoordinate2D(latitude: -34, longitude: 18.5)
+        let span = MKCoordinateSpanMake(0.5, 0.5)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        mapView.addAnnotations(RouteViewController.spotList())
+        
+        mapView.delegate = self
         tableView.delegate = self
         tableView.dataSource = self
+    }
+    
+
+    class Restaurant: NSObject, MKAnnotation {
+        var title: String? = ""
+        var coordinate: CLLocationCoordinate2D = CLLocationCoordinate2D()
+    }
+    
+    let image = UIImage(named: "newPin")
         
+        //Customize pin
+        func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+            let identifier = "myPins"
+            var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier) // as? MKPinAnnotationView
+            
+            
+            if (annotationView == nil){
+                annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                
+                // MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                // annotationView?.pinTintColor = UIColor.blackColor()
+                
+                annotationView!.image = UIImage(named: "newPin")
+                var frame = annotationView!.frame
+                frame.size.height = 44
+                frame.size.width = 25
+                annotationView!.frame = frame
+                
+                annotationView!.canShowCallout = true
+            }
+            
+            annotationView!.annotation = annotation
+            return annotationView
+        }
+        
+        
+        
+        class func spotList() -> [Restaurant] {
+            
+            let spot = Restaurant ()
+            spot.title = "Workshop 17"
+            spot.coordinate = CLLocationCoordinate2D(latitude: -33.906764,longitude: 18.4164983)
+            
+            let spot2 = Restaurant ()
+            spot2.title = "Truth Coffee"
+            spot2.coordinate = CLLocationCoordinate2D(latitude: -33.9281976,longitude: 18.4227045)
+            
+            let spot3 = Restaurant ()
+            spot3.title = "Chop Chop Coffee"
+            spot3.coordinate = CLLocationCoordinate2D(latitude: -33.9271879,longitude: 18.4327055)
+            
+            return [spot,spot2, spot3]
+        }
+    
+    
+    func backAction() {
+        let cityView = CityTripViewController (nibName: "CityTripView", bundle: nil)
+        self.navigationController?.pushViewController(cityView, animated: true)
+        cityView.navigationItem.hidesBackButton = true
     }
     
-        func backAction() {
-            let cityView = CityTripViewController (nibName: "CityTripView", bundle: nil)
-            self.navigationController?.pushViewController(cityView, animated: true)
-            cityView.navigationItem.hidesBackButton = true
-    }
-    
-        func listAction() {
-            let viewcontroller = TripListViewController (nibName: "TripListViewController", bundle: nil)
-            self.navigationController?.pushViewController(viewcontroller, animated: true)
+    func listAction() {
+        let viewcontroller = TripListViewController (nibName: "TripListViewController", bundle: nil)
+        self.navigationController?.pushViewController(viewcontroller, animated: true)
         }
 
     override func didReceiveMemoryWarning() {
@@ -90,9 +154,6 @@ class RouteViewController: UIViewController, MKMapViewDelegate, CLLocationManage
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 150.0
     }
-    
-    
-
     
     
     /*
